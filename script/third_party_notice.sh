af40674b0bfddb5d0da1737a00077fb9cbd4c879
takagiy <takagiy.4dev@gmail.com>
2020-05-17 22:10:16 +0900

    update script to show message describing the third party notice

--- script/third_party_notice.sh
+++ script/third_party_notice.sh
@@ -2,11 +2,16 @@
 
 scripts=$(dirname -- "$0")
 
+echo "This application uses third party libraries which are distributed under the lincenses which differ from the this application's license. Such libraries and their licenses are listed below." | fmt
+
+echo ""
+
 "${scripts}"/collect_dependencies.sh | while read -r repo
 do
   echo "${repo} : https://github.com/${repo}/"
   echo "==== ==== ==== START LICENSE FOR ${repo} ==== ==== ===="
   echo "${repo}" |
-    "${scripts}"/fetch_license.sh
+    "${scripts}"/fetch_license.sh |
+    fmt
   echo "==== ==== ====  END LICENSE FOR ${repo} ==== ==== ===="
 done
